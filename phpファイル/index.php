474bd76bf192af3236592200ae1dfc7a8ecfd9a1
Hashimoto <nh.redshift@gmail.com>
2023-01-06 19:55:43 +0900

    phpファイルの追加

---
+++ phpファイル/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>HTML内にPHPを記述する方法</title>
+</head>
+<body><h1>
+
+<?php
+print("example");
+?>
+
+<?php
+
+//phpinfo();
+
+print("DB Connect Test");
+
+// データベースへの接続に必要な変数を指定
+$host = '192.168.3.12';
+$username = 'naokidb';
+$passwd = 'Arh406@sql';
+$dbname = 'dbtest';
+ 
+// データベースへ接続
+$db = mysqli_connect($host, $username, $passwd, $dbname);
+ 
+// 接続チェック
+if ($db == false) {
+	die('データベースの接続に失敗しました。');
+	print("失敗");
+}else{
+ 
+  echo "データベースの接続に成功しました！ \n";
+　print("成功");
+}
+
+// データベースの接続を閉じる
+mysqli_close($db);
+?>
+
+</h1></body></html>
